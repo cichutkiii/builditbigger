08bde172198fdc8c010e1cfe7ebe339127c71417
silentman11 <silent.r@wp.pl>
2018-07-22 22:06:18 +0200

    first commit

---
+++ JokeTellingLib/src/main/java/preclaw/pl/joketellinglib/JokeClass.java
@@ -0,0 +1,20 @@
+package preclaw.pl.joketellinglib;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JokeClass {
+    ArrayList<String> jokes = new ArrayList<String>();
+
+    public JokeClass(){
+        jokes.add("Q: What does networking seal say?\n" +
+                "\n" +
+                "A: Arp! Arp! Arp!");
+        jokes.add("Q: How many programmers does it take to change a light bulb?\n" +
+                "\n" +
+                "A: None, that's a hardware problem.");
+        jokes.add("There are only 10 types of people in the world: those who understand binary, and those who don't.");
+    }
+
+
+}
